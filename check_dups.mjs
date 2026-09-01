8e0e6c58fe9d5394838f0c189b7fa136d18b3826
Nikcall <Nikcall@users.noreply.github.com>
2026-09-01 13:34:35 +0500

    chore: remove temp debug script

--- check_dups.mjs
+++
@@ -1,4 +0,0 @@
-const res = await fetch('http://localhost:3001/api/catalog')
-const data = await res.json()
-console.log('Keys:', Object.keys(data))
-console.log('Sample:', JSON.stringify(data).slice(0, 500))
